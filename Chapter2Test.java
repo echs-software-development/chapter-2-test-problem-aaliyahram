5b0338c633a1de7a6c589388ca5343387606c5b6
aaliyahram <aaliyahr7235@gmail.com>
2020-11-12 15:34:42 +0000

    tried to solve chapter2test

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,25 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+
+    double myDouble = 9.78;
+    int myInt = (int)
+
+    System.out.println("Enter a number");
+    
+    double num1 = (int)myDouble;
+
+
+    System.out.println("Enter a second number please");
+
+    double num2 = (int)myDouble;
+    System.out.println(myDouble);
+    
+
+    
   
   }
 }
